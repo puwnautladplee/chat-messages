83d0f50d79ef1c8a986fcabcf32eeea988efe078
Ohm <puwnautladplee>
2024-05-07 11:05:57 +0700

    update url socket env

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,8 +1,8 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL: any = 'http://localhost:5555';
-
+const URL: any = process.env.URL_SOCKET ? process.env.URL_SOCKET : 'http://localhost:5555';
+console.log(URL);
 export const socket = io(URL, { 
  autoConnect: false,
  reconnection: true 
